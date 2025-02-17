88ad70f00e56c1f3a978bdca3b252ecaedb27484
Alexandre Akira <alexandreakiraakao@gmail.com>
2025-02-17 15:25:55 -0300

    feat: create page without api

--- 1 - Fundamentos do Desenvolvimento WEB/10 - member-club/src/main.js
+++ 1 - Fundamentos do Desenvolvimento WEB/10 - member-club/src/main.js
@@ -1,3 +1,9 @@
 import "./styles/global.css";
+import "./styles/main.css";
+import "./styles/input.css";
+import "./styles/user-info.css";
+import "./styles/loyalty-card.css";
+import "./styles/counter-cut.css";
+import "./styles/historic-cut.css";
 
 console.log("hello world");
